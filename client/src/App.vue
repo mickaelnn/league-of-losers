4bff07cd93c88965f5455cfb9563133a5e1a44ae
Alan Neves <alanrenoneves@gmail.com>
2021-06-08 22:52:26 -0300

    app and api working!

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ getSummonerData({ summoner: state.summoner }) }}
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+export default {
+  name: "App",
+
+  data() {
+    return {
+      state: {
+        summoner: "AL4N",
+      },
+    };
+  },
+
+  methods: {
+    getSummonerData({ summoner }) {
+      //TODO get user data!
+      return summoner;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
